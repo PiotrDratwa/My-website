af17c8ac0286fbc9c326b1a5112362e0255e45b1
PiotrDratwa <piotr.k.dratwa@gmail.com>
2024-11-21 16:26:09 +0100

    fixed animations, added new one to skill categories buttons and started implementing their functionality

--- client/script.js
+++ client/script.js
@@ -1,14 +1,28 @@
+const observerFactory = (show_class) => {
+    return new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add(show_class);
+            } else {
+                entry.target.classList.remove(show_class);
+            }
+        });
+    });
+};
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add('show')
-        }else{
-            entry.target.classList.remove('show')
-        }
-    })
-})
+function show_skills_tab(){
+    for(e of document.querySelectorAll('.skill_category_button_show')){
+        document.getElementById("skills_categories").removeChild(e);
+    }
+    document.getElementById("123").style.display = "flex";
+}
 
-const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach((el) =>observer.observe(el))
+const hiddenElements = document.querySelectorAll('.hidden');
+const skill_buttons = document.querySelectorAll('.skill_category_button_hidden');
+
+const observer_show = observerFactory("show");
+const observer_skill = observerFactory("skill_category_button_show");
+
+
+hiddenElements.forEach((el) => observer_show.observe(el));
+skill_buttons.forEach((el) => observer_skill.observe(el));
