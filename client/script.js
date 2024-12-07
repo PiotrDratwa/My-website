8ce2e73d5546cbac08554efa756c051de0369146
PiotrDratwa <piotr.k.dratwa@gmail.com>
2024-12-07 17:06:47 +0100

    added showing of all skills buttons and animated them

--- client/script.js
+++ client/script.js
@@ -10,11 +10,6 @@ const observerFactory = (show_class) => {
     });
 };
 
-function show_skills_tab(id){
-    el_style = document.getElementById(id).style;
-    el_style.display = "grid";
-}
-
 const hiddenElements = document.querySelectorAll('.hidden');
 const skill_buttons = document.querySelectorAll('.skill_category_button_hidden');
 
@@ -24,3 +19,26 @@ const observer_skill = observerFactory("skill_category_button_show");
 
 hiddenElements.forEach((el) => observer_show.observe(el));
 skill_buttons.forEach((el) => observer_skill.observe(el));
+
+class skills_tab{
+    last_tab = "";
+
+    show_skills_tab(id){
+        //if there was last shown tab, then hide it so the new one can appear
+        if(this.last_tab !== ""){
+            document.getElementById(this.last_tab).style.display = "none"
+        }
+
+        document.getElementById(id).style.display = "grid";
+        this.last_tab = id
+
+        const children = document.getElementById(id).children;
+        for (const child of children) {
+            child.classList.remove('slide-in');
+            void child.offsetWidth; // Force reflow to restart animation
+            child.classList.add('slide-in');
+        }
+    }
+}
+
+const skill_tab = new skills_tab();
